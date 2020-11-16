905ba4c26353a4d5667a50bc1be0fa45fd22e7b5
teacher Marcos <teacher.mrdv@gmail.com>
2020-11-16 13:32:18 +0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,20 +3,35 @@ public class Fibonacci
 	
 	public static void main (String[] args)
 	{
-		int n = IBIO.inputInt("N = ");
+		int n = IBIO.inputInt("Number of terms to calculate = ");
 		int f = 0;
 		int s = 1;
 		int t = f + s;
+		int counter = 2;
 		
-		while( t <= n )
+		IBIO.out(f + " " + s + " ");
+		// while loop version
+		while( counter <= n )
 		{
-			if( t <= n )
-			{
-				IBIO.out( t + " " );
-			}
+			IBIO.out( t + " " );
 			f = s;
 			s = t;
 			t = f + s;
+			counter++;
+		}
+		
+		IBIO.output("\n---------------------------\n");
+		
+		// for loop version
+		f = 0; // zero-th and
+		s = 1; // first terms of the Fibonacci sequence
+		IBIO.out(f + " " + s + " ");
+		for(int i = 2; i <= n; i++)
+		{
+			t = f + s;
+			IBIO.out(t + " ");
+			f = s;
+			s = t;
 		}
 	}
 }
