56d2dd2dea728dd8efd6d4f9423bb1182227dcd4
teacher Marcos <teacher.mrdv@gmail.com>
2021-03-25 17:08:36 +0800

    Update Queue.java

--- HL1/ADTs/Queue.java
+++ HL1/ADTs/Queue.java
@@ -27,9 +27,14 @@ public class Queue
 	}
 	
 	public int deQueue()
-	{	int data = head.data;
-		head = head.next;
-		return data;
+	{	if(!isEmpty())
+		{	int data = head.data;
+			head = head.next;
+			return data;
+		} else {
+			System.out.println("Error-Empty queue");
+			return 0;
+		}
 	}
 	
 	public void printQueue()
