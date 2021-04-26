4471170846e9cd95bc917449327838255f2b9682
teacher Marcos <teacher.mrdv@gmail.com>
2021-04-26 17:20:21 +0800

    Monday update

--- Arrays/ArrayDemo2.java
+++ Arrays/ArrayDemo2.java
@@ -21,7 +21,7 @@ public class ArrayDemo2
 		{
 			System.out.print( "  " + a[i]);
 		}
-		System.out.printf("\t%d elements in array\n", lastIndex);
+		System.out.printf("\t| %d elements in array\n", lastIndex);
 	}
 	
 	public static void add(int[] a, int data)
@@ -62,38 +62,43 @@ public class ArrayDemo2
 	
 	public static void insert(int[] a, int data, int index)
 	{
-		// code to insert data into a at index "index"
-	}
-	
-	public static void bubble(int[] a)
-	{
-		
+		if( isFull() == true )
+			System.out.println("Error - array full");
+		else {
+			lastIndex++;
+			for(int i = lastIndex-1; i > index; i--)
+			{	a[i] = a[i-1];
+				printArray(a);
+			}
+			a[index] = data;
+		}
+		printArray(a);
 	}
 	
 	public static void main(String[] args)
 	{
 		int[] array = new int[MAXSIZE];
-		System.out.println("HELLO");
-		ArrayDemo.printArray(array);
-		//ArrayDemo.printArray(args);
-		int[] a = {1,3,5,7,9,11,2,4,6,8,12};
+		printArray(array);
+		int[] a = {1,3,5,7,9,11,2,4,6,8,12}; // numbers to input
 		for(int x : a)
 		{	
 			System.out.printf(" >>> Adding %d to array\n", x);
 			add(array, x);
 		}
-		System.out.println("search for 10: " + search(array, 10) );
-		System.out.println("search for 11: " + search(array, 11) );
-		System.out.println("remove(array, 1):");
+		
+		System.out.println("\nsearch for 10: " + search(array, 10) );
+		System.out.println("\nsearch for 11: " + search(array, 11) );
+		System.out.println("\nremove(array, 1):");
 		remove(array,1);
-		System.out.println("remove(array, 8):");
+		System.out.println("\nremove(array, 8):");
 		remove(array,8);
-		System.out.println("remove(array, 11):");
+		System.out.println("\nremove(array, 11):");
 		remove(array,11);
-		System.out.println("remove(array, 12):");
+		System.out.println("\nremove(array, 12):");
 		remove(array,12);
-		sort(array);
-		printArray(array);
+		System.out.println("\ninsert(array, 10, 4);");
+		insert(array, 10, 4);
+
 	}
 }
 
