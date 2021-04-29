5d189f89858a8e3c8752f558c669ffc7cf856698
teacher Marcos <teacher.mrdv@gmail.com>
2021-04-29 18:35:35 +0800

    Thursday update

--- HL1/ADTs/LListDemo.java
+++ HL1/ADTs/LListDemo.java
@@ -9,25 +9,41 @@ public class LListDemo
 	public static void main (String[] args)
 	{
 		LinkedList l = new LinkedList();
-		int[] array = {5,1,4,7,9,1,6,3,2,0};
-		
-		for(int x : array)
+		int[] inputData = {5,1,4,7,9,1,6,3,2,0};
+		System.out.println("Adding data to a linked list:");
+		printArray(inputData);
+		System.out.println();
+		for(int x : inputData)
 		{
 			l.append(x);
 			l.printList();
 		}
-		l.remove(1); l.printList();
+		System.out.println("Removing 5 from the linked list:");
+		l.remove(5); l.printList();
+		System.out.println("Removing 9 from the linked list:");
 		l.remove(9); l.printList();
+		System.out.println("Removing 0 from the linked list:");
 		l.remove(0); l.printList();
 		
-		System.out.println("\n\nSorted Linked List!\n");
+		System.out.println("\n\nSorted Linked List!\nInput data:");
+		printArray(inputData);
 		LinkedList sorted = new LinkedList();
-		for(int x : array)
+		for(int x : inputData)
 		{
 			sorted.insertInOrder(x);
 			sorted.printList();
 		}
 		
+	}
+	
+	public static void printArray(int[] a)
+	{
+		System.out.print( "Array contents -->");
+		for(int i = 0; i < a.length; i++)
+		{
+			System.out.print( "  " + a[i]);
+		}
+		System.out.printf("\t: %d elements in array\n", a.length);
 	}
 }
 
