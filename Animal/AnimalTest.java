6b390b9ee628d6f4b7fa50def1b3a48c8d9d04e9
teacher Marcos <teacher.mrdv@gmail.com>
2021-08-31 15:21:40 +0800

    Animal add

---
+++ Animal/AnimalTest.java
@@ -0,0 +1,12 @@
+public class AnimalTest
+{
+    public static void main(String[] args)
+    {
+        Animal baah = new Animal("Sheep", 'f', 2018);
+        System.out.println(baah);
+        baah.eat();
+        baah.talk();
+
+    }
+
+}
