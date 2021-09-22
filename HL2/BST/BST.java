b26f032aa771b18d3ba812d543b786bfe4cb2b8b
teacher Marcos <teacher.mrdv@gmail.com>
2021-09-22 13:08:54 +0800

    Update BST.java

--- HL2/BST/BST.java
+++ HL2/BST/BST.java
@@ -53,6 +53,42 @@ public class BST
             inOrder(node.getRight());
     }
 
+    private void preOrder(BinaryNode node)
+    {
+        System.out.print(node + " ");
+        if (node.getLeft() != null)
+            preOrder(node.getLeft());
+
+        if (node.getRight() != null)
+            preOrder(node.getRight());
+    }
+
+    private void postOrder(BinaryNode node)
+    {
+        if (node.getLeft() != null)
+            postOrder(node.getLeft());
+
+        if (node.getRight() != null)
+            postOrder(node.getRight());
+
+        System.out.print(node + " ");
+    }
+
+
+    private void descOrder(BinaryNode node)
+    {
+        if (node.getRight() != null)
+            descOrder(node.getRight());
+        System.out.print(node + " ");
+        if (node.getLeft() != null)
+            descOrder(node.getLeft());
+    }
+
+    public boolean search(BinaryNode node)
+    {
+
+
+    }
 
     public static void main(String[] args)
     {
@@ -61,8 +97,31 @@ public class BST
         b.add(3);
         b.add(9);
         b.add(2);
-        System.out.print("InOrder traversal: ");
+        System.out.println("preOrder traversal: ");
+        b.preOrder(b.root);
+        System.out.println("\nInOrder traversal: ");
         b.inOrder(b.root);
+        System.out.println("\npostOrder traversal: ");
+        b.postOrder(b.root);
+        System.out.println("\ndescending Order traversal: ");
+        b.descOrder(b.root);
     }
 
 }
+
+/*
+    BST created with 5 as its root.
+        (12) added to the right of (5)
+        (3) added to the left of (5)
+        (9) added to the left of (12)
+        (2) added to the left of (3)
+        preOrder traversal:
+        (5) (3) (2) (12) (9)
+        InOrder traversal:
+        (2) (3) (5) (9) (12)
+        postOrder traversal:
+        (2) (3) (9) (12) (5)
+        descending Order traversal:
+        (12) (9) (5) (3) (2)
+
+*/
